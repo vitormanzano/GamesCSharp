02993c46ab3da1c45a6f77ecf130262594fd4708
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-05-28 20:40:04 -0300

    Construindo principais classes e criando o desenho no xaml

---
+++ SnakeGameProject/Images.cs
@@ -0,0 +1,19 @@
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SnakeGameProject;
+
+public static class Images
+{
+    public readonly static ImageSource Empty = LoadImage("Empty.png");
+    public readonly static ImageSource Body = LoadImage("Body.png");
+    public readonly static ImageSource Head = LoadImage("Head.png");
+    public readonly static ImageSource Food = LoadImage("Empty.png");
+    public readonly static ImageSource DeadBody = LoadImage("DeadBody.png");
+    public readonly static ImageSource DeadHead = LoadImage("DeadHead.png");
+    
+    private static ImageSource LoadImage(string fileName)
+    {
+        return new BitmapImage(new Uri($"Assets/SnakeAssets/{fileName}", UriKind.Relative));
+    }
+}
