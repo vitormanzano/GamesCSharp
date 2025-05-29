766a3ed75519dbeb858b3235ad3565191799d05d
Vitor Manzano <vimanzanovillela@gmail.com>
2025-05-29 15:07:51 -0300

    Jogo feito

--- SnakeGameProject/GameState.cs
+++ SnakeGameProject/GameState.cs
@@ -9,6 +9,7 @@ public class GameState
     public int Score { get; private set; }
     public bool GameOver { get; private set; }
     
+    private readonly LinkedList<Direction> dirChanges = new LinkedList<Direction>();
     private readonly LinkedList<Position> snakePositions = new LinkedList<Position>(); //First element is the head and the last is the tail
     private readonly Random random = new Random();
 
@@ -42,7 +43,7 @@ public class GameState
             {
                 if (Grid[r, c] == GridValue.Empty)
                 {
-                    yield return new Position(r, c);
+                    yield return new Position(r, c); //retorna apenas o elemento
                 }
             }
         }
@@ -89,8 +90,34 @@ public class GameState
         Grid[tail.Row, tail.Col] = GridValue.Empty;
     }
 
+    private Direction GetLastDirection()
+    {
+        if (dirChanges.Count == 0)
+        {
+            return Dir;
+        }
+        
+        return dirChanges.Last.Value;
+    }
+
+    private bool CanChangeDirection(Direction newDir)
+    {
+        if (dirChanges.Count == 2)
+        {
+            return false;
+        }
+
+        Direction lastDir = GetLastDirection();
+        return newDir != lastDir && newDir != lastDir.Opposite();
+    }
+
     public void changeDirection(Direction dir)
     {
+        if (CanChangeDirection(dir))
+        {
+            dirChanges.AddLast(dir);
+        }
+        dirChanges.AddLast(dir);
         Dir = dir;
     }
 
@@ -115,7 +142,13 @@ public class GameState
     }
 
     public void Move()
-    {   
+    {
+        if (dirChanges.Count > 0)
+        {
+            Dir = dirChanges.First.Value;
+            dirChanges.RemoveFirst();
+        }
+        
         Position newHeadPos = HeadPosition().Translate(Dir);
         GridValue hit = WillHit(newHeadPos);
 
