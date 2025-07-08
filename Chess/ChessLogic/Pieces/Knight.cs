efc159e14e322985871f700702466e0e24cc3f94
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-07-08 20:59:21 -0300

    feat: Adding classes for my pieces
    Adding the abstract class to represent a piece and use to inherit other
    pieces

---
+++ Chess/ChessLogic/Pieces/Knight.cs
@@ -0,0 +1,22 @@
+﻿namespace ChessLogic
+{
+    public class Knight : Piece
+    {
+        public override PieceType Type => PieceType.Knight;
+
+        public override Player Color { get; }
+
+        public Knight(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            Knight copy = new Knight(Color);
+            copy.HasMoved = HasMoved;
+
+            return copy;
+        }
+    }
+}
