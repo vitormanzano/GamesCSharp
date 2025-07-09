4b0976604fe697139c4b09474d78d9191a461bb1
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-07-08 22:39:11 -0300

    feat: Drawing the pieces in the board

---
+++ Chess/ChessLogic/GameState.cs
@@ -0,0 +1,14 @@
+﻿namespace ChessLogic
+{
+    public class GameState
+    {
+        public Board Board { get; set; }
+        public Player CurrentPlayer { get; set; }
+
+        public GameState(Player player, Board board)
+        {
+            CurrentPlayer = player;
+            Board = board;
+        }
+    }
+}
---
+++ Chess/ChessUi/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ChessLogic;
+using ChessUI;
+
+namespace ChessUi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Image[,] pieceImages = new Image[8, 8];
+
+        private GameState gameState;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
+        }
+
+        private void InitializeBoard()
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    Image image = new Image();
+                    pieceImages[r, c] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
+        }
+
+        private void DrawBoard(Board board)
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    Piece piece = board[r, c];
+                    pieceImages[r, c].Source = Images.GetImage(piece);
+                }
+            }
+        }
+    }
+}
