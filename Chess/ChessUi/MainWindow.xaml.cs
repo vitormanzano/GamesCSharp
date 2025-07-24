cece2c8d23016c0ef3dbb1beb185b6523348fcb4
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-07-24 20:59:53 -0300

    feat: Init board

--- Chess/ChessUi/MainWindow.xaml.cs
+++ Chess/ChessUi/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Drawing;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using ChessLogic;
 using ChessUI;
 
@@ -11,8 +13,11 @@ namespace ChessUi
     public partial class MainWindow : Window
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
+        private readonly Rectangle[,] highlights = new Rectangle[8, 8];
+        private readonly Dictionary<Position, Move> moveCache = new Dictionary<Position, Move>();
 
         private GameState gameState;
+        private Position selectedPos = null;
 
         public MainWindow()
         {
@@ -32,6 +37,10 @@ namespace ChessUi
                     Image image = new Image();
                     pieceImages[r, c] = image;
                     PieceGrid.Children.Add(image);
+
+                    Rectangle highlight = new Rectangle();
+                    highlights[r, c] = highlight;
+                    HighlightGrid.Children.Add(highlight);
                 }
             }
         }
@@ -45,6 +54,31 @@ namespace ChessUi
                     Piece piece = board[r, c];
                     pieceImages[r, c].Source = Images.GetImage(piece);
                 }
+            }
+        }
+
+        private void BoardGrid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void CacheMoves(IEnumerable<Move> moves)
+        {
+            moveCache.Clear();
+
+            foreach (Move move in moves)
+            {
+                moveCache[move.ToPosition] = move;
+            }
+        }
+
+        private void ShowHighlights()
+        {
+            Color color = Color.FromArgb(150, 125, 255, 125);
+
+            foreach (Position to in moveCache.Keys)
+            {
+                highlights[to.Row, to.Column].Fill = new SolidColorBrush(color);
             }
         }
     }
