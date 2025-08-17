ed94bbc0c6390ffc12605dcedb0a34747b8e5c1f
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-08-16 21:05:04 -0300

    fix: King and queen were in wrong place

--- Chess/ChessLogic/Board.cs
+++ Chess/ChessLogic/Board.cs
@@ -31,8 +31,8 @@ namespace ChessLogic
             this[0, 0] = new Rook(Player.Black);
             this[0, 1] = new Knight(Player.Black);
             this[0, 2] = new Bishop(Player.Black);
-            this[0, 3] = new King(Player.Black);
-            this[0, 4] = new Queen(Player.Black);
+            this[0, 3] = new Queen(Player.Black);
+            this[0, 4] = new King(Player.Black);
             this[0, 5] = new Bishop(Player.Black);
             this[0, 6] = new Knight(Player.Black);
             this[0, 7]  = new Rook(Player.Black);
@@ -40,8 +40,8 @@ namespace ChessLogic
             this[7, 0] = new Rook(Player.White);
             this[7, 1] = new Knight(Player.White);
             this[7, 2] = new Bishop(Player.White);
-            this[7, 3] = new King(Player.White);
-            this[7, 4] = new Queen(Player.White);
+            this[7, 3] = new Queen(Player.White);
+            this[7, 4] = new King(Player.White);
             this[7, 5] = new Bishop(Player.White);
             this[7, 6] = new Knight(Player.White);
             this[7, 7] = new Rook(Player.White);
